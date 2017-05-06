a46a7c86b04d83128859b226ead5e0e3559d76f5
ronen <ronen@Ronen-PC.pisoft.local>
2017-05-06 16:42:45 +0300

    add args check with user messsage

--- raft-leader-election/src/test/java/io/scalecube/services/leader/election/example/LeaderElectionMain.java
+++ raft-leader-election/src/test/java/io/scalecube/services/leader/election/example/LeaderElectionMain.java
@@ -8,13 +8,17 @@ import io.scalecube.transport.Address;
 public class LeaderElectionMain {
 
   public static void main(String[] args) {
-    
+
+    if (args.length < 2) {
+      System.out.println("the application paramters must set with seed node address ");
+    }
+
     RaftLeaderElection leaderElection1 = new RaftLeaderElection(new Config());
 
     Microservices node1 = Microservices.builder()
-         .seeds(Address.create(args[0], Integer.parseInt(args[1])))
-         .services(leaderElection1).build();
-    
+        .seeds(Address.create(args[0], Integer.parseInt(args[1])))
+        .services(leaderElection1).build();
+
     leaderElection1.start(node1);
   }
 
